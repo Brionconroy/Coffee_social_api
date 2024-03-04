f3d9f62537a74f8904997c64235f3fa4b70828df
Brionconroy <razvanbrion@gmail.com>
2024-03-04 13:55:27 +0000

    Add CLIENT_ORIGIN for front-end

--- coffee_social_api/settings.py
+++ coffee_social_api/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import re
 from pathlib import Path
 import os
 import dj_database_url
@@ -63,7 +64,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['8000-brionconroy-coffeesocia-9oz47r4qwyk.ws-eu108.gitpod.io', 'coffee-social-4aee33ad3a6e.herokuapp.com']
+ALLOWED_HOSTS = ['8000-brionconroy-coffeesocia-9oz47r4qwyk.ws-eu108.gitpod.io', os.environ.get('ALLOWED_HOST'), 'localhost',]
 
 
 # Application definition
@@ -109,9 +110,10 @@ MIDDLEWARE = [
 ]
 
 if 'CLIENT_ORIGIN' in os.environ:
-     CORS_ALLOWED_ORIGINS = [
-         os.environ.get('CLIENT_ORIGIN')
-     ]
+     extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
+     CORS_ALLOWED_ORIGIN_REGEXES = [
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
+    ]
 else:
      CORS_ALLOWED_ORIGIN_REGEXES = [
          r"^https://.*\.gitpod\.io$",
