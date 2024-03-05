45757e40a0991a4e2b82a60f9bf26a87410ae241
Brionconroy <razvanbrion@gmail.com>
2024-03-05 16:46:50 +0000

    ADD rating out of 5 to the reviews model

--- reviews/models.py
+++ reviews/models.py
@@ -4,12 +4,20 @@ from barista.models import Barista
 
 
 class Review(models.Model):
+
+    ONE_TO_FIVE_RATING_CHOICES = (
+    (1, '1'),
+    (2, '2'),
+    (3, '3'),
+    (4, '4'),
+    (5, '5'),
+)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     barista = models.ForeignKey(
         Barista, on_delete=models.CASCADE, related_name='reviews'
     )
     content = models.TextField()
-    rating = models.IntegerField()
+    rating = models.IntegerField(choices=ONE_TO_FIVE_RATING_CHOICES)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
